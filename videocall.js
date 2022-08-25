73ab161e69e5a5b84682e8c94f2f72ad84fd459e
Pratik Pansare <84313956+pratik97669966@users.noreply.github.com>
2022-08-26 01:30:05 +0530

    Update videocall.js

--- videocall.js
+++ videocall.js
@@ -11,7 +11,7 @@ remoteVideo.onplaying = () => { remoteVideo.style.opacity = 1 }
 let peer
 function init(userId) {
      peer = new Peer(userId, {
-           host:'opentalkvoice.herokuapp.com', secure:true, port:443
+            host:'peerjs-server.herokuapp.com', secure:true, port:443
        })
        peer.on('open', () => {
            Android.onPeerConnected()
